d04846ae7b61e9813d5a6c650b117586448e7d48
Adam Pozidis <adchrpoz@gmail.com>
2020-03-03 19:29:45 +0100

    controler and index of doctors

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,14 +1,18 @@
 class DoctorsController < ApplicationController
   def index
-    @doctors = Doctor.all
-    @doctors = Doctor.joins(:specialties).where({ specialties: { name: params[:specialties] } }) if params[:specialties].present?
+    if params[:specialties].present? # We check if the user gave as  a specitly
+      @doctors = Doctor.joins(:specialties).where({ specialties: { name: params[:specialties] } }) # We search for a doctor with this specialty
+    else
+      @doctors = Doctor.all
+    end
+
     @addresses = Doctor.where("address = #{params[:address]}")
   end
 
-  private
+    private
 
-  def search_params
-    params.require(:doctor).permit(:specialty, :location)
+    def search_params
+      params.require(:doctor).permit(:specialty, :location)
+    end
+
   end
-
-end
