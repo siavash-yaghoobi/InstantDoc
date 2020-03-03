762f4b6f7ac521db0c24190179910eeecd8f6091
Adam Pozidis <adchrpoz@gmail.com>
2020-03-03 15:48:56 +0100

    navbar and some routes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,12 @@
 class SearchesController < ApplicationController
+
   def home
   end
+
+  def show
+    @search = Search.new
+    # @specialty = Specialty.new
+    @specialties = Specialty.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'searches/home'
   devise_for :users
   root to: 'searches#home'
+  resources :searches, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
