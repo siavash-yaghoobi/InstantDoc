c9e2736390d032acc7b0f8f87fa5b15a3272910e
Adam Pozidis <adchrpoz@gmail.com>
2020-03-04 12:56:13 +0100

    fixed search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,17 +1,19 @@
 class SearchesController < ApplicationController
+  #before_action :set_specialties, only:
   skip_before_action :authenticate_user!, only: [:home, :index, :show]
 
   def index
 
   end
 
-  def show
-    @search = Search.new
-    @specialty = Specialty.new
-    @specialties = Specialty.all
-  end
   def create
-    raise
+    search = Search.new(search_params)
+  end
+
+  private
+
+  def search_params
+    params[:search].permit(:address, :specialty)
   end
 
 end
