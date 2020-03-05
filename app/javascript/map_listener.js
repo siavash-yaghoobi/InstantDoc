8986d6c6b4d3fed17bf123ea3275adac4243027b
Anna Siregar <siregar.ruliana@gmail.com>
2020-03-05 08:50:29 +0100

    map toggle list

--- app/javascript/map_listener.js
+++ app/javascript/map_listener.js
@@ -1,6 +1,14 @@
-const mapIcon = document.getElementById('map_img');
-console.log(mapIcon);
+const toggleMap = document.getElementById('toggle-map');
 
-function mapClick() {
+function mapToggleEvent() {
+  const map = document.getElementById('map');
+  map.style.display = map.style.display === 'none' ? 'block' : "none";
 
+  const list = document.getElementById('doctor-list');
+  list.style.display = list.style.display === 'none' ? 'block' : "none";
+
+  this.classList.toggle('show-map-icon');
+  this.classList.toggle('show-list-text');
 }
+
+toggleMap.addEventListener('click', mapToggleEvent);
