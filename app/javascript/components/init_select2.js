9c272e7f7057b2590339e500d2efe4a81e175a8e
Anna Siregar <siregar.ruliana@gmail.com>
2020-03-09 19:28:28 +0100

    bunch of frontend

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -12,7 +12,33 @@ const refreshSearch = () => {
   $('#specialty').on('select2:select', function (e) {
     // Do something
     console.log(e)
+
   });
 };
 
 export { initSelect2, refreshSearch };
+
+
+
+// put an event listener on the speciality select tag
+// select.addEventListener('change', (event) => {
+//   event.preventDefault();
+
+
+
+// });
+
+// if the input changes
+
+//   function call
+
+
+//     get  the params of the url
+
+//     get the speciality
+
+//     make a fetch get request with the params and the speciality
+
+//     https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 import '../init_places';
 import "../map_listener";
-import "../radius_slider";
+// import { rangeSlider } from "../radius_slider";
 import 'select2/dist/css/select2.css';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../components/init_mapbox';
@@ -11,6 +11,8 @@ import { initSelect2, refreshSearch } from '../components/init_select2';
 initSelect2();
 refreshSearch();
 initMapbox();
+// rangeSlider();
+
 
 
 
