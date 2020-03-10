d390f1cc48b23bc2cee9395288b3ea654cb3f34a
Anna Siregar <siregar.ruliana@gmail.com>
2020-03-10 16:32:53 +0100

    countdown frontend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import '../init_places';
 import "../map_listener";
+import "../timer.js";
 // import { rangeSlider } from "../radius_slider";
 import 'select2/dist/css/select2.css';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -1,5 +1,6 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+// grab the time from html and change it
+var countDownDate = new Date("March 10, 2020 16:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -11,19 +12,16 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("notif-timer").innerHTML = minutes + "m " + seconds + "s ";
 
   // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("notif-timer").innerHTML = "EXPIRED";
   }
 }, 1000);
 
