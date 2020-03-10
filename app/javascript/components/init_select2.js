370edcc398b149b5d877a01ef3550098d632acb4
Anna Siregar <siregar.ruliana@gmail.com>
2020-03-10 10:04:50 +0100

    dropdown search now works

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -3,20 +3,48 @@ import 'select2';
 
 const initSelect2 = () => {
   $('.select2').select2();
-};
 
-const refreshSearch = () => {
-  const select = document.querySelector('#specialty');
-  console.log(select);
-
-  $('#specialty').on('select2:select', function (e) {
-    // Do something
-    console.log(e)
-
+  // on page load, choose whichever specialty was selected
+  document.addEventListener("DOMContentLoaded", function(event) {
+    const currSpecialty = getQueryParameterByName('specialties');
+    $('#specialty').val(currSpecialty).trigger('change');
   });
+
+  // on specialty select
+  $("#specialty")
+    .on('select2:select', function (e) {
+    // Do something
+      const selectedSpecialtyId = e.params.data.id;
+      window.location.href = updateQueryStringParameter(window.location.href, 'specialties', selectedSpecialtyId);
+    });
 };
 
-export { initSelect2, refreshSearch };
+
+
+function getQueryParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function updateQueryStringParameter(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    return uri + separator + key + "=" + value;
+  }
+}
+
+
+
+export { initSelect2 };
 
 
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,10 @@ import "../map_listener";
 import 'select2/dist/css/select2.css';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../components/init_mapbox';
-import { initSelect2, refreshSearch } from '../components/init_select2';
+import { initSelect2 } from '../components/init_select2';
 
 
 initSelect2();
-refreshSearch();
 initMapbox();
 // rangeSlider();
 
