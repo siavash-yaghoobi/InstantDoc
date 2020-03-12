eac997841f0280759aa9b13e59030303801cad6e
Anna Siregar <siregar.ruliana@gmail.com>
2020-03-12 17:53:15 +0100

    new navs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,10 +9,9 @@ import 'select2/dist/css/select2.css';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../components/init_mapbox';
-
-
 initSelect2();
 initMapbox();
+
 // rangeSlider();
 // import { rangeSlider } from "../radius_slider";
 
